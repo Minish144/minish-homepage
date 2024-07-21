9ee67e1f10c21da7d8d01f4c88f0fdf618a68da5
Nikolai Varlamov <varlamow.col@yahoo.com>
2024-07-21 19:02:17 +0300

    feat(menu): add github sources link

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,24 @@
 import { Link } from "@chakra-ui/next-js";
 import { Container } from "@chakra-ui/react";
+import GithubIcon from "./icons/github";
 
 function Menu() {
   return (
-    <Container className="fixed z-50 w-full p-3 font-bold text-lg items-center backdrop-blur-md">
+    <Container className="flex justify-between fixed z-50 w-full p-3 items-center backdrop-blur-md">
       <Link
         href="/"
-        className="ml-2 hover:text-gray-400"
+        className="text-md font-bold hover:text-gray-400"
         sx={{ textDecoration: "none", ":hover": { textDecoration: "none" } }}
       >
-        🤖 Minish144
+        Nikolay Varlamov
+      </Link>
+      <Link
+        href="https://github.com/Minish144/minish-homepage"
+        isExternal={true}
+        className="flex items-center ml-2 hover:text-gray-400"
+      >
+        <GithubIcon />
+        <span className="ml-1">Source</span>
       </Link>
     </Container>
   );
