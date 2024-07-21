c4fa970ed6dd787a46b6b2c2edb8b1a056bad7cf
Nikolai Varlamov <varlamow.col@yahoo.com>
2024-07-21 18:53:37 +0300

    feat(font): set custom font

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,5 +1,14 @@
+"use client";
+
+import Fonts from "@/components/Fonts";
+import Theme from "@/components/Theme";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export function Providers({ children }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return (
+    <>
+      <Fonts />
+      <ChakraProvider theme={Theme}>{children}</ChakraProvider>
+    </>
+  );
 }
