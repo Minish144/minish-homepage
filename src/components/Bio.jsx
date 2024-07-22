206cb3c3c2db56e20ece476f9791c3a482a53aad
Nikolai Varlamov <varlamow.col@yahoo.com>
2024-07-22 15:34:03 +0300

    feat(bio): update bio order

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,12 +4,23 @@ import TitledLayout from "./layouts/titled";
 function Bio() {
   return (
     <TitledLayout title="Bio">
-      <Box className="mb-3 flex">
+      <Box className="mb-4 flex">
         <Box className="mr-5 font-bold">2001</Box>
         <Box>Born in Moscow, Russia</Box>
       </Box>
 
-      <Box className="mb-3 flex">
+      <Box className="mb-4 flex">
+        <Box className="mr-4">
+          <Box className="font-bold">2019-</Box>
+          <Box className="font-bold">2023</Box>
+        </Box>
+        <Box>
+          Completed a bachelor&apos;s program in applied informatics at the
+          Bauman Moscow State Technical University
+        </Box>
+      </Box>
+
+      <Box className="mb-4 flex">
         <Box className="mr-4">
           <Box className="font-bold">2021-</Box>
           <Box className="font-bold">2022</Box>
@@ -17,20 +28,12 @@ function Bio() {
         <Box>Worked at Qoollo</Box>
       </Box>
 
-      <Box className="mb-3 flex">
+      <Box className="flex">
         <Box className="mr-4">
           <Box className="font-bold">2022-</Box>
           <Box className="font-bold">now</Box>
         </Box>
         <Box>Working at Samokat.tech</Box>
-      </Box>
-
-      <Box className="flex">
-        <Box className="mr-5 font-bold">2023</Box>
-        <Box>
-          Completed a bachelor&apos;s program in applied informatics at the
-          Bauman Moscow State Technical University
-        </Box>
       </Box>
     </TitledLayout>
   );
