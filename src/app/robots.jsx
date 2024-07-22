8da42d56fb42b159b6d683c1470b36bf887b5735
Nikolai Varlamov <varlamow.col@yahoo.com>
2024-07-22 11:56:25 +0300

    feat(seo): add robots

---
+++ src/app/robots.jsx
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: "https://www.minish.ru/sitemap.xml",
+  };
+}
