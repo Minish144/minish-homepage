a0e4e9ab7f384031306f57428c191c588b502199
Nikolai Varlamov <varlamow.col@yahoo.com>
2024-07-27 21:27:47 +0300

    feat(translations): remove locale detection

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -2,10 +2,9 @@ import createMiddleware from "next-intl/middleware";
 import { locales, defaultLocale, defaultMW } from "./i18n";
 
 export default createMiddleware({
-  // A list of all locales that are supported
+  localeDetection: false,
+  localePrefix: "never",
   locales: locales,
-
-  // Used when no locale matches
   defaultLocale: defaultLocale,
 });
 
